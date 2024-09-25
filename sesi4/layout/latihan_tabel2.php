8392244fef28f5f28bd3622caf78e9313c8932b0
Nanda F <nandaftrs61@gmail.com>
2024-09-25 22:01:59 +0700

    commited

---
+++ sesi4/layout/latihan_tabel2.php
@@ -0,0 +1,69 @@
+<!-- 
+    * Pemrograman Web Dasar
+    * latihan_tabel2.php
+    * Nama : Nanda Fitri Safitriani
+    * NIM : 43230287
+    * Tanggal : 23 Agustus 2024
+    * Program Studi : Sistem Informasi
+ -->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Latihan 2</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th rowspan="2" bgcolor="#a52a2a">No</th>
+        <th rowspan="2" bgcolor="#add8e6">Nama</th>
+        <th colspan="5" bgcolor="#00ff00">Pertemuan</th>
+      </tr>
+      <tr>
+        <th bgcolor="00ffff">1</th>
+        <th bgcolor="00ffff">2</th>
+        <th bgcolor="00ffff">3</th>
+        <th bgcolor="00ffff">4</th>
+        <th bgcolor="00ffff">5</th>
+      </tr>
+      <tr align="center">
+        <td>1</td>
+        <td>Neymar</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>s</td>
+      </tr>
+      <tr align="center">
+        <td>2</td>
+        <td>Cristiano</td>
+        <td>a</td>
+        <td>✔</td>
+        <td>i</td>
+        <td>s</td>
+        <td>✔</td>
+      </tr>
+      <tr>
+        <td align="center">3</td>
+        <td>Bale</td>
+        <td align="center">a</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+      </tr>
+      <tr align="center">
+        <td>4</td>
+        <td>Nanda F</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+        <td>✔</td>
+      </tr>
+    </table>
+  </body>
+</html>
